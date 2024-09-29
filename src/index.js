283bb822854d55a9622dda5fe828c7d6a5b7f2ab
Adam Khomsi <adamkhomsi@Adams-MacBook-Air.local>
2024-09-29 16:34:45 -0700

    removed report thing

--- src/index.js
+++ src/index.js
@@ -3,25 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// Create the root element for the React app
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// Register the service worker if supported
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/service-worker.js")
-    .then((registration) => {
-      console.log(
-        "ServiceWorker registration successful with scope: ",
-        registration.scope
-      );
-    })
-    .catch((error) => {
-      console.error("ServiceWorker registration failed: ", error);
-    });
-}
